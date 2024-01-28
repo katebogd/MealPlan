af761f9de606e48db74380bb5cfe93fd3d90bb1e
Ubuntu <ubuntu@ip-172-31-21-87.us-west-1.compute.internal>
2024-01-28 03:12:51 +0000

    Modified opening db

--- cs50.py
+++ cs50.py
@@ -4,10 +4,14 @@ import os
 class SQL:
     def __init__(self, filename):
         self._filename = filename
-        self._connection = sqlite3.connect(os.path.join('/www_data/', filename.replace("sqlite:///", "")))
-        self._cursor = self._connection.cursor()
+        
 
     def execute(self, statement, *args):
-        self._cursor.execute(statement, args)
+        connection = sqlite3.connect(
+            os.path.join(
+                '/www_data/',
+                self._filename.replace("sqlite:///", "")))
+        cursor = connection.cursor()
+        cursor.execute(statement, args)
         return self.cursor.fetchall()
 
